4faf2ece2ed290a96a8da390d9fb45abeaab1ef3
Martin Zachrison <zac@cyberzac.se>
2011-07-26 08:49:40 +0200

    Corrected naming

--- src/main/scala/org/bitcex/MatcherActor.scala
+++ src/main/scala/org/bitcex/MatcherActor.scala
@@ -1,14 +1,19 @@
 package org.bitcex
 
 import akka.actor.Actor
-import model.{AskOrder, Price, SEK, BTC}
+import model.{BidOrder, AskOrder, Price}
 
 //Todo where to send the trades
 class MatcherActor[T <: Price[T], S <: Price[S]] extends Actor {
   val matcher = new Matcher[T, S]()
   protected def receive = {
+
     case askOrder:AskOrder[T, S] => {
-      //  matcher.matchOrder(askOrder)
+        val trades = matcher.matchOrder(askOrder)
+    }
+
+    case bidOrder:BidOrder[T, S] => {
+        val trades = matcher.matchOrder(bidOrder)
     }
   }
 }
