75831b873bfa6c430140c396d5c0e66b9fee8f61
Martin Zachrison <zac@cyberzac.se>
2011-08-07 14:44:42 +0200

    Added test of instance create method

--- src/test/scala/org/bitcex/model/OrderSpec.scala
+++ src/test/scala/org/bitcex/model/OrderSpec.scala
@@ -5,12 +5,20 @@ import akka.actor.Actor._
 
 class OrderSpec extends Specification {
 
-  "AskOrder" should {
+  val user = User(Name("Nisse"), Email("mail"), UserId("1"), Password("pw"))
+  val userRef = actorOf(new UserActor(user))
+  val askOrderSEK = AskOrderSEK(BTC(10), SEK(5), userRef)
+
+  "AskOrderSEK" should {
     "Have a total" in {
-      val user = User(Name("Nisse"), Email("mail"), UserId("1"), Password("pw"))
-      val userRef = actorOf(new UserActor(user))
-      AskOrderSEK(BTC(10), SEK(5), userRef).total must_== SEK(50)
+      askOrderSEK.total must_== SEK(50)
+    }
+
+    "Have a create method" in {
+      val newOrder = askOrderSEK.create(BTC(4))
+      newOrder.amount must_== BTC(4)
+      newOrder.price must_== SEK(5)
+      newOrder.sellerRef must_== userRef
     }
   }
-
 }
